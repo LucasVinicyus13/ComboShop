bc3e96ea46065cd64bd4315856fe350bec9bf089
LucasVinicyus13 <sanches.anacleto@escola.pr.gov.br>
2025-06-18 16:00:04 -0300

    Update donate.js

--- donate/donate.js
+++ donate/donate.js
@@ -3,39 +3,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const doacoes = [
     {
-      imagem: "./images/Produto1.jpeg",
-      nome: "Bebê Reborn",
-      descricao: "Uma criança que você não tera nenhum trabalho para cuidar.",
-      preco: 129.90,
-      detalhes: "O Bebê Reborn é feito com silicone e roupas costuradas à mão. Ideal para presentes ou colecionadores.",
+      imagem: "./images/qrcode10.jpeg",
+      nome: "R$10,00",
     },
     {
-      imagem: "./images/Produto2.jpg",
-      nome: "Fraldas Pampers",
-      descricao: "Fraldas para o seu Bebê Reborn sempre ficar limpinho.",
-      preco: 79.90,
-      detalhes: "Contém 138 unidades. Conforto seco garantido por até 12h.",
+      imagem: "./images/qrcode25.jpeg",
+      nome: "R$25,00",
     },
     {
-      imagem: "./images/Produto3.png",
-      nome: "Sapatênis",
-      descricao: "Sapatênis tamanho 38 para ficar muito no estilo.",
-      preco: 90.00,
-      detalhes: "Confeccionado em material sintético de alta qualidade, com palmilha macia e solado antiderrapante para máximo conforto e segurança.",
+      imagem: "./images/qrcode50.jpeg",
+      nome: "R$50,00",
     },
     {
-      imagem: "./images/Produto4.jpg",
-      nome: "Boné do PT",
-      descricao: "Compre este boné do PT para estar sempre apoiando nosso querido presidente.",
-      preco: 13.13,
-      detalhes: "Boné ajustável com estampa bordada em destaque, feito em algodão resistente para garantir estilo e durabilidade no apoio ao seu partido.",
+      imagem: "./images/qrcode75.jpeg",
+      nome: "R$75,00",
     },
     {
-      imagem: "./images/Produto5.jpeg",
-      nome: "Tapete da MC Pipokinha",
-      descricao: "Esse tapete impede de suas visitas chegarem na sua casa e falar mal da pipokinha.",
-      preco: 69.00,
-      detalhes: "Tapete em tecido antiderrapante, com estampa ousada e divertida da MC Pipokinha. Ideal para quem quer recepcionar as visitas com muito estilo (e um toque de polêmica).",
+      imagem: "./images/qrcode100.jpeg",
+      nome: "R$100,00",
     }
   ];
 
@@ -45,9 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
     card.innerHTML = `
       <img src="${doacoes.imagem}" alt="${doacoes.nome}">
       <h2>${doacoes.nome}</h2>
-      <p>${doacoes.descricao}</p>
-      <span class="price">R$ ${doacoes.preco.toFixed(2)}</span>
-      <button type="button" class="btn-comprar">Comprar</button>
     `;
 
     card.querySelector(".btn-comprar").addEventListener("click", () => {
