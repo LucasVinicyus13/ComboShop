13ec489b5c70559296bc2e0b23a0d6eafe444ad3
Lucas Vinicyus <sanches.anacleto@escola.pr.gov.br>
2025-06-24 15:10:10 -0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMpbU5K-LpvnDqG-2UOncbbOMSijch19c",
+  authDomain: "comboshop-66b1c.firebaseapp.com",
+  projectId: "comboshop-66b1c",
+  storageBucket: "comboshop-66b1c.appspot.com",
+  messagingSenderId: "607173380854",
+  appId: "1:607173380854:web:60b02791198cdc113e7ad7"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.fazerLogin = async function (event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value;
+
+  if (!username || !password) {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  const fakeEmail = username + "@comboshop.com";
+
+  try {
+    await signInWithEmailAndPassword(auth, fakeEmail, password);
+    alert("Login realizado com sucesso!");
+    window.location.href = "https://combo-shop.vercel.app/products/produtos.html";
+  } catch (error) {
+    console.error("Erro no login:", error.message);
+    alert("Usuário ou senha inválidos. Tente novamente.");
+  }
+};
